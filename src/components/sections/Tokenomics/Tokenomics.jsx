3843e1600df033f2899730fed728af423edd94e2
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-03 12:17:12 +0600

    aa

---
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const Tokenomics = () => {
+  return (
+    <div className='lg:mt-[200px]'>
+      <Container>
+        <p className='text-[#FBA32C] font1 text-[115px] lg:leading-[138px] tracking-[2.3px] text-center lg:mb-[100px]'>TOKENOMICS</p>
+
+        <div>
+
+          {/* tokenomics text section  */}
+          <div>
+            <p className='text-[#FBA32C] font1 lg:text-[40px] lg:leading-[48px] lg:tracking-[0.4px]'>nO TAX. NO NONSENSE</p>
+          </div>
+
+        </div>
+
+      </Container>
+    </div>
+  );
+};
+
+export default Tokenomics;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import AboutUs from '@/components/sections/AboutUs/AboutUs';
 import Faq from '@/components/sections/Faq/Faq';
 import FooterLinkSection from '@/components/sections/FooterLinkSection/FooterLinkSection';
 import Roadmap from '@/components/sections/Roadmap/Roadmap';
+import Tokenomics from '@/components/sections/Tokenomics/Tokenomics';
 import { Factory } from 'lucide-react';
 import React from 'react';
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <div>
       <AboutUs />
+      <Tokenomics />
       <Roadmap />
       <Faq />
       <FooterLinkSection />
