7840e539084bb339371762a9e68c221cae7b0a0d
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-03 08:01:06 +0600

    aa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import AboutUs from '@/components/sections/AboutUs/AboutUs';
+import Faq from '@/components/sections/Faq/Faq';
+import { Factory } from 'lucide-react';
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <AboutUs />
+      <Faq />
     </div>
   );
 };
