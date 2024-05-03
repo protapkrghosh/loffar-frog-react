eb90310e37fa7a766b693179c7cb559a5607e3dc
Protap Ghosh <protapg518@gmail.com>
2024-05-04 02:51:27 +0600

    s

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -52,7 +52,7 @@ const Banner = () => {
     }
     timerComponents.push(
       <span key={interval} className="relative">
-        <span className="text-[#1E1510] text-[15.99px] font-semibold font-pantonRust rounded-[8px] mx-[20px] xl:mx-[24px] 2xl:mx-[26px] py-3">
+        <span className="text-[#1E1510] text-[16px] lg:text-[15px] xl:text-[15.99px] font-semibold font-pantonRust rounded-[8px] mx-[20px] xl:mx-[24px] 2xl:mx-[26px] py-3">
           {timeLeft[interval]}
         </span>{" "}
 
@@ -81,22 +81,22 @@ const Banner = () => {
   return (
     <div>
       <div>
-        <img src={bannerBg} alt="Image" className=" xl:h-[1150px] 2xl:h-auto w-full xl:-mt-[125px] xl:-mb-[928px] 2xl:-mb-[1050px]" />
+        <img src={bannerBg} alt="Image" className="h-[1000px] lg:h-[1150px]  xl:h-[1150px] 2xl:h-auto w-full -mt-[125px] -mb-[800px] lg:-mb-[950px] xl:-mb-[928px] 2xl:-mb-[1050px]" />
 
         <div className="mb-[235px]">
           <Container>
-            <div className="md:flex justify-between items-center gap-x-[91px]">
-              <div className="md:w-[40%] xl:w-[50%] 2xl:w-[40%]">
+            <div className="md:flex justify-between items-center gap-x-[35px] xl:gap-x-[91px]">
+              <div className="md:w-[60%] xl:w-[50%] 2xl:w-[40%]">
                 <div className="relative">
-                  <img src={frogCard} alt="Image" className="xl:h-[820px] 2xl:h-[820px] xl:scale-105 2xl:scale-110"/>
+                  <img src={frogCard} alt="Image" className="lg:h-[840px] xl:h-[820px] 2xl:h-[820px] xl:scale-105 2xl:scale-110"/>
 
-                  <div className="absolute bottom-[40px] 2xl:bottom-[25px] left-[90px] 2xl:left-[95px]">
-                    <p className="text-[#1E1510] text-[15px] font-pantonRust font-extrabold leading-[19px] proportional-nums">Presale Ends in:</p>
-                    <p className="text-[#1E1510] text-[14px] font-passeroOne leading-[16px] mt-[9px] mb-[11px]">Token Will Be On Raydium My 1fst 2024</p>
+                  <div className="absolute bottom-[40px] lg:bottom-[48px] xl:bottom-[40px] 2xl:bottom-[25px] left-[90px] lg:left-[100px] xl:left-[90px] 2xl:left-[95px]">
+                    <p className="text-[#1E1510] text-[15px] lg:text-[15px] xl:text-[15px] font-pantonRust font-extrabold leading-[19px] proportional-nums">Presale Ends in:</p>
+                    <p className="text-[#1E1510] text-[14px] lg:text-[13px] xl:text-[14px] font-passeroOne leading-[16px] mt-[9px] mb-[11px]">Token Will Be On Raydium My 1fst 2024</p>
 
                     {/* Timers */}
                     <div className="relative">
-                      <div className="h-[58px] xl:w-[260px] 2xl:w-[265px] bg-[#FBA32C] rounded-[8px] xl:-ml-[18px] 2xl:-ml-[18px]" />
+                      <div className="h-[58px] w-[235px] xl:w-[260px] 2xl:w-[265px] bg-[#FBA32C] rounded-[8px] xl:-ml-[18px] 2xl:-ml-[18px]" />
                       {timerComponents.length ?
                         (
                           <div className="absolute -top-1">
@@ -104,13 +104,13 @@ const Banner = () => {
                               {timerComponents.length ? timerComponents : <span>Time's up!</span>}
                             </div>
 
-                            <div className="text-[15px] text-[#1E1510] font-pantonRust font-semibold leading-[19px] space-x-14 xl:space-x-[63px] 2xl:space-x-[68px] absolute top-[16px] left-[55px] xl:left-[40px] 2xl:left-[40px]">
+                            <div className="text-[15px] text-[#1E1510] font-pantonRust font-semibold leading-[19px] space-x-14 lg:space-x-[55px] xl:space-x-[63px] 2xl:space-x-[68px] absolute top-[16px] left-[55px] xl:left-[40px] 2xl:left-[40px]">
                               <span>:</span>
                               <span>:</span>
                               <span>:</span>
                             </div>
 
-                            <div className="text-[8.6px] text-[#1E1510] font-passeroOne leading-[10px] space-x-[45px] xl:space-x-[53px] 2xl:space-x-[56px] absolute top-[43px] left-[25px] xl:left-[4px] 2xl:left-[4px]">
+                            <div className="text-[8.6px] text-[#1E1510] font-passeroOne leading-[10px] space-x-[45px] lg:space-x-[44px] xl:space-x-[53px] 2xl:space-x-[56px] absolute top-[43px] left-[25px] xl:left-[4px] 2xl:left-[4px]">
                               <span>Day</span>
                               <span>Hrs</span>
                               <span>Mins</span>
@@ -122,33 +122,33 @@ const Banner = () => {
                     </div>
 
                     <div>
-                      <p className="bg-[#efc79b] text-[#1E1510] text-center text-[14px] font-passeroOne leading-[16px] tracking-[0.56px] rounded-[6px] pr-3 py-[6px] mt-[18px] mb-3 -ml-4 xl:-ml-9 2xl:-ml-12 w-fit capitalize"><span className="bg-[#FBA32C] pl-3 pr-[5px] py-[8px] rounded-[6px]">over 2 Million</span> Contribution Received and Growing</p>
+                      <p className="bg-[#efc79b] text-[#1E1510] text-center text-[13px] xl:text-[14px] font-passeroOne leading-[16px] tracking-[0.56px] rounded-[6px] pr-3 py-[6px] mt-[18px] mb-3 -ml-6 lg:-ml-9 xl:-ml-9 2xl:-ml-12 w-fit capitalize"><span className="bg-[#FBA32C] pl-3 pr-[5px] py-[8px] rounded-[6px]">over 2 Million</span> Contribution Received and Growing</p>
 
-                      <p className="text-[#624737] text-[18px] font-pantonRust font-medium leading-[21px] ml-4 2xl:ml-0">Send Sol To This Wallet</p>
+                      <p className="text-[#624737] text-[17px] xl:text-[18px] font-pantonRust font-medium leading-[21px] ml-3 xl:ml-4 2xl:ml-0">Send Sol To This Wallet</p>
 
                       {/* Copied text */}
-                      <div className='flex justify-center md:justify-start -ml-4 xl:-ml-9 2xl:-ml-12 my-3'>
-                        <div className="flex justify-center items-center text-[14px] border border-[#d88c28] bg-[#FBA32C] py-[9px] rounded-[6px] w-fit px-[11px]">
+                      <div className='flex justify-center md:justify-start -ml-4 lg:-ml-9 xl:-ml-9 2xl:-ml-12 my-3'>
+                        <div className="flex justify-center items-center text-[13px] xl:text-[14px] border border-[#d88c28] bg-[#FBA32C] py-[9px] rounded-[6px] w-fit px-[11px]">
                           <p className="text-[#1E1510] font-passeroOne mr-2 md:mr-3">0xaa86d4542969389b57936be83e5c888de001a95e</p>
                           <button onClick={() => { setCopied(!copied); copyLink() }}>
                             {
-                              copied ? <MdLibraryAddCheck className="text-[13px] cursor-pointer text-[#1E1510]" /> :
-                                <FiCopy className="text-[13px] cursor-pointer text-[#1E1510]" />
+                              copied ? <MdLibraryAddCheck className="text-[12px] xl:text-[13px] cursor-pointer text-[#1E1510]" /> :
+                                <FiCopy className="text-[12px] xl:text-[13px] cursor-pointer text-[#1E1510]" />
                             }
                           </button>
                         </div>
                       </div>
 
-                      <p className="text-[#624737] text-[18px] font-pantonRust font-medium leading-[21px] xl:-ml-3 2xl:-ml-6">HOW TO PARTICIPATE IN PRESALE</p>
+                      <p className="text-[#624737] text-[17px] xl:text-[18px] font-pantonRust font-medium leading-[21px] lg:-ml-[15px] xl:-ml-3 2xl:-ml-6">HOW TO PARTICIPATE IN PRESALE</p>
 
-                      <div className="text-[#1E1510] text-[13px] 2xl:text-[14px] text-center font-passeroOne leading-[16px] tracking-[0.56px] capitalize mt-[8px] mb-[12px] -ml-[55px] 2xl:-ml-[75px]">
+                      <div className="text-[#1E1510] text-[12px] xl:text-[13px] 2xl:text-[14px] text-center font-passeroOne leading-[16px] tracking-[0.56px] capitalize mt-[8px] mb-[12px] lg:-ml-[58px] xl:-ml-[55px] 2xl:-ml-[75px]">
                         <p className="bg-[#efc69b91] border-[0.5px] border-[#EDBB8D] rounded-[6px] py-[6px]">Send Sol to our provided wallet.</p>
 
                         <p className="bg-[#efc69b91] border-[0.5px] border-[#EDBB8D] rounded-[6px] py-[6px] my-[6px]">Minimum: 0.50 Sol and Maximum 150 Sol From A Single Wallet.</p>
 
                         <p className="bg-[#efc69b91] border-[0.5px] border-[#EDBB8D] rounded-[6px] py-[6px]">tokens will be airdropped to the sending wallet.</p>
 
-                        <p className="bg-[#FBA32C] border-[0.6px] border-[#d88c28] rounded-[6px] px-3 2xl:px-[10px] py-[6px] mt-3">NB: Don't use a Exchange to send SOL! Use your own wallet..</p>
+                        <p className="bg-[#FBA32C] border-[0.6px] border-[#d88c28] rounded-[6px] px-3 2xl:px-[10px] py-[6px] mt-3 lg:mt-2 xl:mt-3">NB: Don't use a Exchange to send SOL! Use your own wallet..</p>
                       </div>
                     </div>
                   </div>
@@ -156,18 +156,19 @@ const Banner = () => {
               </div>
 
               {/* Banner Content */}
-              <div className="md:w-[60%] xl:w-[50%] 2xl:w-[60%]">
-                <h3 className="text-[60px] xl:text-[50px] 2xl:text-[60px] text-[#FBA32C] font-pantonRust leading-[72px] tracking-[1.2px]">Welcome to</h3>
-                <h1 className="text-[65px] xl:text-[77px] 2xl:text-[116px] text-[#FBA32C] font-pantonRust 2xl:leading-[150px] tracking-[2.5px] -mt-3 2xl:mt-[8px]">Loffar frog</h1>
+              <div className="md:w-[40%] xl:w-[50%] 2xl:w-[60%]">
+                <h3 className="text-[60px] lg:text-[27px] xl:text-[40px] 2xl:text-[60px] text-[#FBA32C] font-pantonRust leading-[72px] tracking-[1.2px]">Welcome to</h3>
 
-                <p className="xl:text-[15px] 2xl:text-[24px] text-[#B2ACA8] font-passeroOne xl:leading-[35px] 2xl:leading-[40px] tracking-[0.48px] 2xl:mt-[24px] mb-[40px] 2xl:mb-[50px] xl:w-[95%] 2xl:w-full">Where imagination leaps beyond boundaries, crafting worlds of whimsy and wonder. Dive into our pond of creativity and join the adventure today!</p>
+                <h1 className="text-[65px] lg:text-[48px] xl:text-[77px] 2xl:text-[116px] text-[#FBA32C] font-pantonRust 2xl:leading-[150px] tracking-[2.5px] -mt-3 2xl:mt-[8px]">Loffar frog</h1>
 
-                <div className="space-x-7">
-                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] 2xl:px-[24px] py-2 2xl:py-[12px] duration-100'>BUY ON OKX</button>
+                <p className="lg:text-[15px] xl:text-[15px] 2xl:text-[24px] text-[#B2ACA8] font-passeroOne lg:leading-[25px] xl:leading-[35px] 2xl:leading-[40px] tracking-[0.48px] lg:mt-[15px] xl:mt-0 2xl:mt-[24px] mb-[40px] 2xl:mb-[50px] xl:w-[95%] 2xl:w-full">Where imagination leaps beyond boundaries, crafting worlds of whimsy and wonder. Dive into our pond of creativity and join the adventure today!</p>
 
-                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] 2xl:px-[24px] py-2 2xl:py-[12px] duration-100'>BUY ON MEXC</button>
+                <div className="space-x-2 xl:space-x-7">
+                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[13px] xl:text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] lg:px-3 xl:px-[20px] 2xl:px-[24px] py-3 lg:py-1 xl:py-2 2xl:py-[12px] duration-100'>BUY ON OKX</button>
 
-                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] 2xl:px-[24px] py-2 2xl:py-[12px] duration-100'>BUY ON UNISWAP</button>
+                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[13px] xl:text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] lg:px-3 xl:px-[20px] 2xl:px-[24px] py-3 lg:py-1 xl:py-2 2xl:py-[12px] duration-100'>BUY ON MEXC</button>
+
+                  <button className='border border-[#FBA32C] hover:bg-[#fba12c15] text-[#FBA32C] font-passeroOne text-[13px] xl:text-[15px] 2xl:text-[20px] lg:leading-[32px] lg:tracking-[0.4px] rounded-[8px] px-[20px] lg:px-3 xl:px-[20px] 2xl:px-[24px] py-3 lg:py-1 xl:py-2 2xl:py-[12px] duration-100'>BUY ON UNISWAP</button>
                 </div>
               </div>
             </div>
