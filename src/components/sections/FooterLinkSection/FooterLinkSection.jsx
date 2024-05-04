f8d692677164477a761029551e6006c93b0e8dc8
Protap Ghosh <protapg518@gmail.com>
2024-05-04 08:00:04 +0600

    s

--- src/components/sections/FooterLinkSection/FooterLinkSection.jsx
+++ src/components/sections/FooterLinkSection/FooterLinkSection.jsx
@@ -12,15 +12,14 @@ const FooterLinkSection = () => {
       <Container>
 
         {/* First part footer  */}
-        <div className='flex justify-between lg:pt-[88px] lg:pb-[70px]'>
+        <div className='flex justify-between xl:pt-[88px] xl:pb-[70px] py-[55px]'>
 
           <div>
             <img className='w-auto' src={footerLogo} alt="image" />
           </div>
 
           <div className='flex lg:gap-[32px] items-center'>
-
-            <div className='flex gap-[14px]'>
+            <div className='flex gap-[14px] mr-6 lg:mr-0'>
               <a href="" target='_blank' className='text-[#F7B501] px-3 py-3 border border-[#F7B501] hover:bg-[#301F0C] rounded-[5px]'><FaTwitter className='w-[27px] h-[27px]' /></a>
 
               <a href="" target='_blank' className='text-[#F7B501] px-3 py-3 border border-[#F7B501] hover:bg-[#301F0C] rounded-[5px]'><FaTelegramPlane className='w-[27px] h-[27px]' /></a>
--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -6,15 +6,15 @@ const Footer = () => {
     <div className='bg-[#FBA32C]'>
       <Container>
         {/* second part footer  */}
-        <div className='flex justify-between items-center py-[33px]'>
+        <div className='flex flex-col md:flex-row justify-center md:justify-between items-center py-[20px] md:py-[33px]'>
 
           <div className='flex items-center gap-[40px]'>
-            <p className='text-[#1E1510] font-passeroOne text-[20px] leading-[36px] uppercase'>loffar frog © {new Date().getFullYear()}</p>
-            <p className='text-[#1E1510] font-passeroOne text-[20px] leading-[36px] uppercase cursor-pointer'>hi@loffar frog.com</p>
+            <p className='text-[#1E1510] font-passeroOne lg:text-[20px] leading-[36px] uppercase'>loffar frog © {new Date().getFullYear()}</p>
+            <p className='text-[#1E1510] font-passeroOne lg:text-[20px] leading-[36px] uppercase cursor-pointer'>hi@loffar frog.com</p>
           </div>
 
           <div>
-            <p className='text-[#1E1510] font-passeroOne text-[20px] leading-[32px] uppercase'>© 2023 , KINGDOM OF KEK, All Rights Reserved</p>
+            <p className='text-[#1E1510] font-passeroOne lg:text-[20px] leading-[32px] uppercase'>© 2023 , KINGDOM OF KEK, All Rights Reserved</p>
           </div>
         </div>
       </Container>
