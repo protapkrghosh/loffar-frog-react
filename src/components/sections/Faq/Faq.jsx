7026f596fea0a42e6b80d00f5617683d822f6336
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-05-04 23:16:40 +0600

    aa

--- src/components/sections/Faq/Faq.jsx
+++ src/components/sections/Faq/Faq.jsx
@@ -7,11 +7,10 @@ const Faq = () => {
     <div className='my-[60px] md:my-[100px] xl:mt-[430px] lg:mt-[350px] md:mt-[160px] mt-[90px]' id='partners'>
       <Container>
 
-        <div className='lg:flex 2xl:space-x-[150px] xl:space-x-[90px] gap-x-10 xl:gap-x-0'>
+        <div className='flex flex-col-reverse lg:flex-row 2xl:space-x-[150px] xl:space-x-[90px] gap-x-10 xl:gap-x-0'>
 
           {/* left side */}
-          <div className='lg:w-[55%]'>
-
+          <div className='lg:w-[55%] mt-10 lg:mt-0'>
             {/* card 1  */}
             <div className='border border-[#775A34] bg-[#301F0C] px-7 py-5 xl:py-7 rounded-[16px] mb-[30px]'>
               <p className='text-[#FBA32C] font-passeroOne text-[20px] xl:text-[24px] leading-[33.6px] pb-[15px] lg:pb-[12px] xl:pb-[18px] uppercase'>What is Loffar Frog?</p>
@@ -37,6 +36,10 @@ const Faq = () => {
               <p className='text-[#B2ACA8] font-passeroOne text-[15px] xl:text-[17px] 2xl:text-[18px] leading-[28.8px] capitalize xl:w-[95%]'>Stay connected with Loffar Frog by subscribing to our newsletter and following us on social media for the latest news and updates.</p>
             </div>
 
+            <div className='2xl:w-[700px] xl:w-[500px] mt-14 lg:mt-0 block lg:hidden'>
+              <img className='xl:ml-[-60px] lg:h-[500px] xl:h-[600px] 2xl:h-auto md:w-[70%] lg:w-auto mx-auto lg:mx-0' src={faqImg} alt="image" />
+            </div>
+
           </div>
 
           {/* right side  */}
@@ -49,7 +52,7 @@ const Faq = () => {
             <p className='text-[#B2ACA8] text-center lg:text-start font-passeroOne text-[17px] lg:text-[19px] xl:text-[17px] 2xl:text-[20px] lg:leading-[34px] lg:tracking-[0.4px] lg:mb-[50px] xl:w-[90%] -mt-4 lg:mt-0' data-aos="fade-up"
               data-aos-duration="2200">Explore our FAQ section for quick answers to common queries about Loffar Frog, your gateway to creativity and innovation.</p>
 
-            <div className='2xl:w-[700px] xl:w-[500px] mt-10 lg:mt-0'>
+            <div className='2xl:w-[700px] xl:w-[500px] mt-10 lg:mt-0 hidden lg:block'>
               <img className='xl:ml-[-60px] lg:h-[500px] xl:h-[600px] 2xl:h-auto md:w-[70%] lg:w-auto mx-auto lg:mx-0' src={faqImg} alt="image" />
             </div>
 
